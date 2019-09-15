038f4d42c0aaeb99ffe54cfe3b41cce6e3f93f24
Sergio Rosa <scarlosjazz@gmail.com>
2019-09-15 10:20:45 -0400

    HTTP ERROR 400

--- purenodeserver.js
+++ purenodeserver.js
@@ -1,9 +1,22 @@
 const http = require('http');
+const url = require('url');
 
 function handler(req, res) {
-    res.writeHead(200, {'Content-type': 'text/plain'});
-    res.write('Hello, I am a webserver!');
-    res.end();
+
+    // console.log(req.url);
+    const parsedUrl = url.parse(req.url, true);
+    // console.log(parsedUrl);
+
+
+    if(parsedUrl.pathname === '/'){
+        res.writeHead(200, {'Content-type': 'text/plain'});
+        res.write('Hello, I am a webserver!');
+        res.end();
+    } else {
+        //HTTP ERROR 400
+        res.writeHead(400, {'Content-type': 'text/plain'});
+        res.end();
+    }
 }
 
 const server = http.createServer(handler);
